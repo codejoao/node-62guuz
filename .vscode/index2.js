dbd806f3207fe6389023816455042d5d42f22cb0
codejoao <jaumjonis@hotmail.com>
2022-06-16 15:58:49 -0300

    2 files added

---
+++ .vscode/index2.js
@@ -0,0 +1,14 @@
+const RedesSociais = [
+  'Instagram', 
+  'Facebook' ,
+  'Youtube' ,
+  'LinkedIn' ,
+  'Tiktok' ,
+  'Whatsapp' ,
+  'Twitter' ,
+];
+/*
+const tiktok = RedesSociais.indexOf('Tiktok'); // indexOf utiliza-se para indicar o posição no Array
+console.log(tiktok); //deve mostrar o numero do indice que ela se encontra
+*/
+console.log(RedesSociais.includes('Whatsapp')); // deve informar com boolean se no o parametro esta incluido no Array
---
+++ index2.js
@@ -0,0 +1,12 @@
+const RedesSociais = [
+  'Instagram', 
+  'Facebook' ,
+  'Youtube' ,
+  'LinkedIn' ,
+  'Tiktok' ,
+  'Whatsapp' ,
+  'Twitter' ,
+];
+
+const tiktok = RedesSociais.indexOf('Tiktok'); // indexOf utiliza-se para indicar o posição no Array
+console.log(tiktok); //deve mostrar o numero do indice que ela se encontra
