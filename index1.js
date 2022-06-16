3000122cc198cfe2dc430f8943710879d0ba3c60
codejoao <jaumjonis@hotmail.com>
2022-06-16 16:11:06 -0300

    1 file modified

--- index1.js
+++ index1.js
@@ -7,8 +7,14 @@ const RedesSociais = [
   'Whatsapp' ,
   'Twitter' ,
 ];
-/*
+
 const tiktok = RedesSociais.indexOf('Tiktok'); // indexOf utiliza-se para indicar o posição no Array
 console.log(tiktok); //deve mostrar o numero do indice que ela se encontra
-*/
+
 console.log(RedesSociais.includes('Whatsapp')); // deve informar com boolean se no o parametro esta incluido no Array
+
+//Loppings
+
+for(let i = 0; i < RedesSociais.leagth; i++){
+  console.log(RedesSociais[i]);
+}
