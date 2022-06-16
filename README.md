af3c9b536af662b8842c9689859f97887bc4ebb7
codejoao <jaumjonis@hotmail.com>
2022-06-16 15:39:22 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-62guuz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-62guuz)
---
+++ index.js
@@ -0,0 +1,37 @@
+//Tipos de Dados (Todos Primitivos, exeto o Objeto)
+/*const sou_solteiro = true
+const anoNascimento = 1999
+const media = (9+9+10) /4
+const nome = 'João'
+let idade = null
+const filho
+const numeroGrande = 2151236215123n
+const numeroGrande2 = BigInt("12356541321521212")
+const meuSimbolo = new Synmbol()
+const meuObjeto = {
+  Casa: "Minha casa no Guarujá"
+  Moradores: ['João', 'Maria', 'José']
+}
+*/
+/* Estrutura de Dados que voce aprenderá
+- Arrays
+- Objetos
+- Listas 
+- Listas encadeadas
+- Pilhas, filas e deques
+- Conjuntos
+- Dicionario
+- Tabela hash
+*/
+
+// 1 - Arrays
+// QUANDO FALAMOS DE ARRAYS: ESTAMOS FALANDO DE VETORES E MATRIZES
+// UM VETOR NADA MAIS É QUE UM ARRAY UNIDIMENCIONAL, OU SEJA UMA VARIAVEL QUE ARMAZENA VARIOS DADOS DO MESMO TIPO.//
+// UMA MATRIX É UM ARRAY MULTIDIMENCIONAL, OU SEJA UM VETOR DE VETORES.//
+// AS ARRAYS SÓ PODEM ARMAZENAR DADOS DE UM MESMO TIPO, PORÉM O JS CONSEGUE UTILIZAR MAIS DE UM TIPO DE DADO NO MESMO ARRAY.//
+
+const saldoGabriel = 1000;
+const saldoVanessa = 10000;
+
+const total = saldoGabriel + saldoVanessa;
+console.log(total);
